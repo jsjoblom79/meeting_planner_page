bd501059e8fb0e33fac7ee2e48d9d49664f22bcb
jsjoblom79 <jsjoblom@me.com>
2025-04-15 18:18:55 -0600

    finalized the app

--- meeting_planner/meetings/urls.py
+++ meeting_planner/meetings/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('<int:id>', views.detail, name="detail"),
-    path('rooms', views.all_rooms, name="rooms")
+    path('rooms', views.all_rooms, name="rooms"),
+    path('new', views.new, name="new"),
+    
 ]
